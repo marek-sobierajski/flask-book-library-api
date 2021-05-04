bcbc0eee6ede607e6044e2c3ef785eb92f7865ec
Marek <marek-sobierajski@wp.pl>
2021-05-05 00:11:55 +0200

    initial version

---
+++ book_library.py
@@ -0,0 +1,10 @@
+from book_library_app import create_app
+
+app = create_app()
+
+
+
+# from book_library_app import app
+
+# if __name__ == "__main__":
+#     app.run()
---
+++ book_library_app/auth/__init__.py
@@ -0,0 +1,4 @@
+from flask import Blueprint
+
+auth_bp = Blueprint('auth', __name__)
+from book_library_app.auth import auth
